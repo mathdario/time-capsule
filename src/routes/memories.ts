030c759b06064f31fc735eb4d12bdb87704d5777
Matheus <matheus_dario@outlook.com>
2023-05-18 21:01:33 -0300

    feat: :sparkles: added new routes

---
+++ src/routes/memories.ts
@@ -0,0 +1,43 @@
+import fastify, { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get("/memories", async () => {
+    const memories = await prisma.memory.findMany({
+      orderBy: {
+        createAt: "asc",
+      },
+    });
+
+    return memories.map((memory) => {
+      return {
+        id: memory.id,
+        coverUrl: memory.converUrl,
+        excerpt: memory.content.substring(0, 115).concat("..."),
+      };
+    });
+  });
+
+  app.get("/memories:id", async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = paramsSchema.parse(request.params);
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    });
+
+    return memory;
+  });
+
+  app.post("/memories", async () => {});
+
+  app.put("/memories:id", async () => {});
+
+  app.delete("/memories:id", async () => {});
+}
